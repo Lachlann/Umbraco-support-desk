3aa0ffea700cf770f202e677aedcf2f1b27bf129
Dreamguts <cliff.lipp@gmail.com>
2016-08-21 21:53:37 +0100

    added repo and controller

---
+++ src/CP.UmbracoSupportDesk/CP.UmbracoSupportDesk/Api/FaqController.cs
@@ -0,0 +1,28 @@
+﻿
+using CP.UmbracoSupportDesk.POCOS;
+using CP.UmbracoSupportDesk.Repository;
+using System.Net.Http;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace CP.UmbracoSupportDesk.Api
+{
+    public class FaqController
+    {
+        public HttpResponseMessage AddBooking(Faq faqItem)
+        {
+            HttpContext.Current.Response.ContentType = "application/json";
+            Faq createdItem = Faqs.Save(faqItem);
+            if (createdItem != null)
+            {
+                HttpContext.Current.Response.StatusCode = 200;  
+            }
+            else {
+                HttpContext.Current.Response.StatusCode = 500;
+            }
+            HttpContext.Current.Response.Write(new JavaScriptSerializer().Serialize(createdItem));
+            return new HttpResponseMessage();
+        }
+
+    }
+}
---
+++ src/CP.UmbracoSupportDesk/CP.UmbracoSupportDesk/Repository/Faqs.cs
@@ -0,0 +1,22 @@
+﻿using CP.UmbracoSupportDesk.POCOS;
+using System.Collections.Generic;
+using Umbraco.Core.Persistence;
+using System;
+
+
+namespace CP.UmbracoSupportDesk.Repository
+{
+    public class Faqs
+    {
+        public static Faq Save(Faq item)
+        {
+            UmbracoDatabase db = Umbraco.Core.ApplicationContext.Current.DatabaseContext.Database;
+            using (var uow = db.GetTransaction())
+            {
+                db.Save(item);
+                uow.Complete();
+                return item;
+            }
+        }
+    }
+}
