7d2d5abf128f6888781089d0cc17a883d5a300a1
Dreamguts <cliff.lipp@gmail.com>
2016-08-21 16:49:49 +0100

    added app plugin

---
+++ src/CP.UmbracoSupportDesk/CP.USD.Umbraco/App_Plugins/SupportDesk/SupportDesk.controller.js
@@ -0,0 +1,6 @@
+﻿var SupportDeskCntrl = angular.module("umbraco");
+
+
+SupportDeskCntrl.controller("SupportDesk", function ($scope, $filter, $http, $routeParams, editorState, contentResource, notificationsService, navigationService, appState, $timeout) {
+
+});
